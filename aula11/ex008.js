786850febd27774fc18595fb50dc3e064735cc7c
Andre Mikael Siqueira <107883589+AndreMikaelSiqueira@users.noreply.github.com>
2023-01-04 13:38:25 -0300

    04/01/23 13:38

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+console.log(`A Velocidade Do Seu Carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    (console.log('Voce Ultrapassou a velocidade permitada. MULTADO!'))
+} else {
+    (console.log('Dirija Sempre Com Segurança.'))
+}
+    
